8b9bce7d757501b878d6d36e6d11072be235f5e3
Omar <omaar_5@hotmail.com>
2021-08-20 15:07:07 +0200

    setup: reducers and actions

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,3 +1,6 @@
 import { combineReducers } from "redux";
+import { shopReducer } from "./shopping/shoppingReducers";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+    shop: shopReducer
+});
