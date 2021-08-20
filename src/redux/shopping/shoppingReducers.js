e71828efab23a8f9edc099a4b141b350af9e503a
Omar <omaar_5@hotmail.com>
2021-08-20 16:44:33 +0200

    done: Reducers

--- src/redux/shopping/shoppingReducers.js
+++ src/redux/shopping/shoppingReducers.js
@@ -37,16 +37,36 @@ const initial_state = {
 export const shopReducer = (state = initial_state, action) => {
     switch (action.type) {
         case actionTypes.ADD_TO_CART:
-            return {};
+            // Get the items data from the products array
+            const item = state.products.find(product => product.id === action.payload.id);
+            // Check if item is already in cart
+            const inCart = state.cart.find(item => item.id === action.payload.id ? true : false);
+            return {
+                ...state,
+                cart: inCart ? state.cart.map(item => (
+                    item.id === action.payload.id ? { ...item, qty: item.qty + 1 } : item
+                )) : [...state.cart, { ...item, qty: 1 }]
+            };
 
         case actionTypes.REMOVE_FROM_CART:
-            return {};
+            return {
+                ...state,
+                cart: state.cart.filter(item => item.id !== action.payload.id)
+            };
 
         case actionTypes.ADJUST_QTY:
-            return {};
+            return {
+                ...state,
+                cart: state.cart.map(item => (
+                    item.id === action.payload.id ? { ...item, qty: action.payload.qty } : item
+                ))
+            };
 
         case actionTypes.LOAD_CURRENT_ITEM:
-            return {};
+            return {
+                ...state,
+                currentItem: action.payload
+            };
 
         default:
             return state;
